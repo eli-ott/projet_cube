8d78bf8aa5d2874a9aad686172fe529c64eceaf5
eli-ott <eliottbidault@gmail.com>
2023-11-22 14:54:51 +0100

    Ajout d'une nouvelle route

--- Program.cs
+++ Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/add", () => "Ajout d'une nouvelle info");
 
 app.Run();
