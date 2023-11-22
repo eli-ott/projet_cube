d60f76bc66f47eb0a9f8a02863516400618db686
eli-ott <eliottbidault@gmail.com>
2023-11-22 18:02:30 +0100

    Added response variable

--- front/script/main.js
+++ front/script/main.js
@@ -2,5 +2,7 @@ document.getElementById('formulaire').addEventListener('submit', async (event) =
     event.preventDefault()
 
     const res = await fetch('http://localhost:5023/');
-    console.log(await res.text());
+    const ret = await res.text();
+
+    console.log(ret);
 });
