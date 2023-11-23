8798d7faf7afbc977a10a179ac3be28befa999ae
eli-ott <eliottbidault@gmail.com>
2023-11-23 14:13:48 +0100

    get city position

--- front/script/main.js
+++ front/script/main.js
@@ -3,33 +3,41 @@ let infoContainer = document.getElementById('infos-container');
 let info = document.getElementById('infos');
 let retour = document.getElementById('retour');
 
+const API_PATH = "http://localhost:5023/";
+
 formulaire.addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    console.log(info);
     info.innerHTML = '';
 
-    //récupération du code postal
-    let codePostal = document.getElementById('code-postal').value;
+    let ret;
 
-    let res, ret;
-    //vérifie si l'input est un code postal ou un nom de ville
-    if(parseInt(codePostal) > 1) {
-        res = await fetch(`http://localhost:5023/citiesinzipcode-${codePostal}`);
-        ret = await res.json();
-    } else {
-        //fetch city name ?
+    let defaultPath = '/front/pages/'
+    let currentPath = window.location.pathname;
+
+    switch (currentPath) {
+        case defaultPath + 'getVille.html':
+            ret = await getVille();
+
+            //affiche la réponse API
+            ret.forEach((ville, i) => {
+                info.innerHTML += `${ville.toUpperCase()}`;
+
+                if (ret.length > 1 && ret.length - 1 !== i) {
+                    info.innerHTML += '&nbsp;&mdash;&nbsp;';
+                }
+            });
+            break;
+        case defaultPath + 'getPos.html':
+            ret = await getPos();
+            console.log(ret);
+
+            //affiche la réponse API
+            info.innerHTML += `lat:&nbsp;${ret.lat}&nbsp;-&nbsp;&nbsp;long:&nbsp;${ret.lng}`;
+            break;
     }
 
     //la réponse de l'api
-    console.log(ret);
-    ret.forEach((ville, i) => {
-        info.innerHTML += `${ville.toUpperCase()}`;
-
-        if (ret.length > 1 && ret.length - 1 !== i) {
-            info.innerHTML += '&nbsp;&mdash;&nbsp;';
-        }
-    });
 
     infoContainer.style.display = 'flex';
     formulaire.style.display = 'none';
@@ -40,3 +48,24 @@ retour.addEventListener('click', () => {
     infoContainer.style.display = 'none';
     formulaire.style.display = 'flex';
 });
+
+const slugify = (city) => {
+    city.replace(/[-']/gm, ' ');
+
+    return city.toLowerCase();
+}
+
+const getVille = async () => {
+    //récupération du code postal
+    let codePostal = document.getElementById('code-postal').value;
+
+    let res = await fetch(`${API_PATH}citiesinzipcode-${codePostal}`);
+    return await res.json();
+}
+const getPos = async () => {
+    let codePostal = document.getElementById('code-postal').value.trim();
+    let slug = document.getElementById('slug').value.toLowerCase().trim();
+
+    let res = await fetch(`${API_PATH}citypos-${codePostal + slug}`)
+    return await res.json();
+}
