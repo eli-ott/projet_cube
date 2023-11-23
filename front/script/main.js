e4f2d7dce0dbfa691cf087042f185fd0a7e60990
eli-ott <eliottbidault@gmail.com>
2023-11-23 10:44:50 +0100

    Show the user the according cities

--- front/script/main.js
+++ front/script/main.js
@@ -1,13 +1,37 @@
-document.getElementById('formulaire').addEventListener('submit', async (event) => {
+let formulaire = document.getElementById('formulaire');
+let infoContainer = document.getElementById('infos-container');
+let info = document.getElementById('infos');
+let retour = document.getElementById('retour');
+
+formulaire.addEventListener('submit', async (event) => {
     event.preventDefault();
+
+    console.log(info);
+    info.innerHTML = '';
 
     //récupération du code postal
     let codePostal = document.getElementById('code-postal').value;
 
     //fetch l'api en fonction du code postal
     const res = await fetch(`http://localhost:5023/${codePostal}`);
-    const ret = await res.text();
+    const ret = await res.json();
 
     //la réponse de l'api
     console.log(ret);
+    ret.forEach((ville, i) => {
+        info.innerHTML += `${ville.toUpperCase()}`;
+
+        if (ret.length > 1 && ret.length - 1 !== i) {
+            info.innerHTML += '&nbsp;&mdash;&nbsp;';
+        }
+    });
+
+    infoContainer.style.display = 'flex';
+    formulaire.style.display = 'none';
+});
+
+retour.addEventListener('click', () => {
+    formulaire.reset();
+    infoContainer.style.display = 'none';
+    formulaire.style.display = 'flex';
 });
