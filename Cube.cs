391c65959416426a06973887bf27b4e1bc7c3a82
Lastelle DUPONT <lastelle.dupont@gmail.com>
2023-11-23 09:37:37 +0100

    Ajout de l'obtention des villes lié à un code

--- Cube.cs
+++ Cube.cs
@@ -15,19 +15,32 @@ namespace Cube {
             connection.UserName     = "root";
             connection.Password     = "ESuKyuERu#2023";
 
+            Dictionary<string, List<string>> zipCodes = new ();
+
             if (connection.IsConnect()) {
                 
-                string query = "SELECT zip_code, name FROM cities";
+                string query = "SELECT zip_code, slug FROM cities";
                 MySqlCommand    command = new (query, connection.Connection);
                 MySqlDataReader reader  = command.ExecuteReader();
                 while(reader.Read()) {
 
-                    // On vérifie que les données ne soient pas nulles
-                    if (!reader.IsDBNull(0) && !reader.IsDBNull(1)) {
+                    const int ZIP_CODE_ID = 0;
+                    const int SLUG_ID     = 1;
 
-                        string someStringFromColumnZero = reader.GetString("zip_code");
-                        string someStringFromColumnOne  = reader.GetString("name");
-                        Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+                    // On vérifie que les données ne soient pas nulles
+                    if (!reader.IsDBNull(ZIP_CODE_ID) && !reader.IsDBNull(SLUG_ID)) {
+
+                        string zipCode  = reader.GetString(ZIP_CODE_ID);
+                        string cityName = reader.GetString(SLUG_ID).Replace(' ', '_'); // On remplace les espaces par des '_'
+
+                        // On vérifie s'il y a déjà un code postal enregistré
+                        if (zipCodes.TryGetValue(zipCode, out List<string>? cityNamesOrNull)) {
+
+                            // Si la liste n'est pas initialisée, on en créé une
+                            if (cityNamesOrNull is List<string> cityNames) cityNames.Add(cityName);
+                            else zipCodes[zipCode] = new () { cityName };
+
+                         } else zipCodes.Add(zipCode, new () { cityName });
 
                     } // if ..
                 } // while ..
@@ -35,6 +48,16 @@ namespace Cube {
                 connection.Close();
 
             } // if ..
+
+            app.MapGet("/{zipCOde}", (string zipCode) => {
+
+                // On vérifie s'il y a déjà un code postal enregistré
+                zipCodes.TryGetValue(zipCode, out List<string>? cityNamesOrNull);
+
+                // S'il n'y a aucune ville liée, on sort une liste avec un message comme valeur
+                return cityNamesOrNull ?? new() { "Aucune ville" };
+                
+            }); // ..
 
             app.Run();
 
