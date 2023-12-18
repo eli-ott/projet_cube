2ee56e9fb6be21c777cf534f322acf4176d4e333
Justine Loriot <loriot.justine@gmail.com>
2023-12-18 15:38:43 +0100

    Routes creation

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Projet Cube (préparation)
 ## Requêtes
 ### GET
-
+- `/measures-{idAppareil}` → renvoi toutes les mesures de l'appareil
+  ```json
+  "/measures-15" : [
+    {"measure":"15","date":"18-12-2023"},
+    {"measure":"18","date":"18-12-2023"},
+    {"measure":"8","date":"18-12-2023"}
+  ]
+  ```
+  `/lastmeasure-{idAppareil}` → renvoi la dernière mesure de l'appareil
+  ```json
+  "/lastmeasure-15" : [
+    {"measure":"8","date":"18-12-2023"}
+  ]
+  ```
 ### POST
+- `/newmeasure` → Ajoute une nouvelle mesure
+  ```json
+  "/newmeasures" : [
+    {
+    "id_mesure": "12",
+    "valeur": "12",
+    "instant": 18-12-2023,
+  }
+  ]
+  ```
--- api/Cube.cs
+++ api/Cube.cs
@@ -4,6 +4,12 @@ using Cube.Data;
 using MySql.Data.MySqlClient;
 
 namespace Cube {
+
+    public class Measure {
+        public float Mesure;
+        public string Date;
+    }
+
     public class Program {
         public static void Main(string[] args) {
 
@@ -32,6 +38,32 @@ namespace Cube {
                 app.Run();
 
             } // void ..
+
+            string requete = String.Format("{\"Mesure\": {0}, \"Date\": {1}, \"IdAppareil\": {2} }", 0f, "dsdsd", 15);
+            Console.WriteLine("test");
+
+            GetAllMeasures(app);
+            GetLastMeasure(app);
+            PostMeasure(app);
+
+            static void GetAllMeasures(WebApplication app){
+                app.MapGet("/measures-{idAppareil}", (int idAppareil) => {
+                    
+                }); //app.MapGet ..
+            } //void ..
+
+            static void GetLastMeasure(WebApplication app){
+                app.MapGet("/lastmeasure-{idAppareil}", (int idAppareil) => {
+
+                }); //app.MapGet ..
+            } //void ..
+
+            static void PostMeasure(WebApplication app){
+                app.MapPost("/newmeasure", (Measure measure) => {
+                    
+                }); //app.MapGet ..
+            } //void ..
+
         } // List<string> ..
     } // class ..
 } // namespace ..
