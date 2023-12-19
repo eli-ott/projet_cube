6bf16bee79ef2d39c24c3e85ad1593f8d50d24c1
Lastelle DUPONT <lastelle.dupont@gmail.com>
2023-12-19 16:44:25 +0100

    Documentation de la connexion MySQL

--- api/DBConnection.cs
+++ api/DBConnection.cs
@@ -3,37 +3,62 @@ using MySql.Data.MySqlClient;
 namespace Cube.Data {
     public class DBConnection {
 
-        private DBConnection() {}
+        //=====================
+        // D E F I N I T I O N
+        //=====================
 
-        public string? Server;
-        public string? DatabaseName;
-        public string? UserName;
-        public string? Password;
+            /** <summary> Nom du serveur. </summary> **/                   public string? Server;
+            /** <summary> Nom de la base de donnée. </summary> **/         public string? DatabaseName;
+            /** <summary> Nom de l'administrateur. </summary> **/          public string? UserName;
+            /** <summary> Mot de passe de l'administrateur. </summary> **/ public string? Password;
 
-        public MySqlConnection? Connection { get; set;}
+            /** <summary> Reference de la connexion au serveur. </summary> **/ public MySqlConnection? Connection { get; set;}
 
-        private static DBConnection? _instance = null;
+            /** <summary> Instance de connexion au serveur. </summary> **/ private static DBConnection? _instance = null;
 
-        public static DBConnection Instance()  {
-           DBConnection. _instance ??= new DBConnection();
-           return _instance;
-        } // DBConnection ..
+
+        //=============================
+        // I M P L E M E N T A T I O N
+        //=============================
+
+            /// <summary>
+            /// Permet d'accéder à l'instance de connexion et de la créer si nécessaire.
+            /// </summary>
+            /// <returns></returns>
+            public static DBConnection Instance()  {
+                DBConnection. _instance ??= new DBConnection();
+                return DBConnection._instance;
+            } // DBConnection ..
+
     
-        public bool IsConnect() {
-            if (Connection == null) {
+            /// <summary>
+            /// Indique si la connexion est établie.
+            /// </summary>
+            /// <returns></returns>
+            public bool IsConnect() {
+                if (this.Connection == null) {
 
-                if (string.IsNullOrEmpty(DatabaseName))
-                    return false;
+                    if (string.IsNullOrEmpty(this.DatabaseName))
+                        return false;
+                    
+                    this.Connection = new MySqlConnection(string.Format("Server={0}; database={1}; UID={2}; password={3}", Server, DatabaseName, UserName, Password));
+                    this.Connection.Open();
+
+                    ConsoleLogger.LogInfo("Connexion avec succès avec la base de donnée.");
+
+                } // if ..
+        
+                return true;
                 
-                Connection = new MySqlConnection(string.Format("Server={0}; database={1}; UID={2}; password={3}", Server, DatabaseName, UserName, Password));
-                Connection.Open();
+            } // bool ..
 
-            } // if ..
     
-            return true;
-        } // bool ..
-    
-        public void Close() => this.Connection?.Close();
-
+            /// <summary>
+            /// Permet de fermer la connexion.
+            /// </summary>
+            public void Close() {
+                this.Connection?.Close();
+                ConsoleLogger.LogWarning("Connexion avec la base de donnée interrompue avec succès.");
+            } // void ..
     } // class ..
 } // namespace ..
