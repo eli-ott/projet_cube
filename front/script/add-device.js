59d9306084cc9ecfa6c1b71071285d2a0662cd16
eli-ott <eliottbidault@gmail.com>
2023-12-19 16:02:35 +0100

    reset changes

--- front/script/add-device.js
+++ front/script/add-device.js
@@ -1,43 +1,15 @@
-function create64BitInteger(ipv4, id) {
-
-    // On converti l'IPV4 sous sa forme de 4 octet
-    const binaryIP = BigInt('0b' + ipv4
-        .split('.')
-        .map(octet => {
-            return parseInt(octet)
-                .toString(2)
-                .padStart(8, '0');
-        } // ..
-    ).join(''));
-
-
-    // On combine l'ID avec l'IPV4
-    const shiftedId = BigInt(id) << BigInt(32);
-    return shiftedId | binaryIP;
-
-} // function ..
-
-
-// Example usage
-const ipv4 = "192.168.1.1";
-const id = 12345;
-const result = create64BitInteger(ipv4, id);
-
-console.log(result.toString()); // Output as string
-
-
 /**
  * Adding a new device using the API when the user clicks on 'Envoyer'
  */
 const addDevice = async () => {
-    let id_appareil = document.getElementById('id_appareil').value;
-    let nom_appareil = document.getElementById('nom_appareil').value;
-    let id_type_mesure = document.getElementById('id_type_mesure').value;
+    const id_appareil = document.getElementById('id_appareil');
+    const nom_appareil = document.getElementById('nom_appareil');
+    const id_type_mesure = document.getElementById('id_type_mesure');
 
     const data = {
-        "idAppareil": id_appareil,
-        "nomAppareil": nom_appareil,
-        "idType": id_type_mesure
+        "idAppareil": id_appareil.value,
+        "nomAppareil": nom_appareil.value,
+        "idType": id_type_mesure.valuee
     }
 
     const res = await fetch('http://localhost:5023/device', {
@@ -53,9 +25,9 @@ const addDevice = async () => {
     } else {
         alert('Impossible d\'ajouter l\'appareil');
     }
-    id_appareil = '';
-    nom_appareil = '';
-    id_type_mesure = '';
+    id_appareil.value = '';
+    nom_appareil.value = '';
+    id_type_mesure.value = '';
 }
 
 document.getElementById('submit').addEventListener('click', addDevice);
