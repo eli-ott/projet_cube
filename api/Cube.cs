fe165f9e6a8f6418828e3dbdcb5cea25196ae6e2
Justine Loriot <loriot.justine@gmail.com>
2023-12-19 15:20:34 +0100

    GetAllMesures

--- api/Cube.cs
+++ api/Cube.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 using Cube.Data;
 using MySql.Data.MySqlClient;
 
@@ -9,6 +10,15 @@ namespace Cube {
         /** <summary> Nombre de secondes depuis 1970-01-01T00:00:00Z </summary> **/  public long  instant    { get; set; }
         /** <summary> Concatenation de l'IPV4 et ID de l'appareil </summary> **/     public int   idAppareil { get; set; }
     } // class ..
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MeasuresValues{
+        /** <summary>  </summary> **/ public string valeurs;
+        /** <summary>  </summary> **/ public string instants;
+        /** <summary>  </summary> **/ public string type;
+    }
 
 
     public class Program {
@@ -40,7 +50,7 @@ namespace Cube {
 
             static void GetAllMeasures(WebApplication app){
                 app.MapGet("/measures-{idAppareil}", (int idAppareil) => {
-                    
+                    FindAllMeasures(idAppareil);
                 }); //app.MapGet ..
             } //void ..
 
@@ -58,6 +68,31 @@ namespace Cube {
 
         } // void ..
 
+        /// <summary>
+        /// Récupère toutes les mesures d'un appareil dans la base de donnée
+        /// </summary>
+        /// <param name="idAppareil"> L'identifiant de l'appareil recherché </param>
+        static Dictionary<string, MeasuresValues> FindAllMeasures(int idAppareil){
+
+            DBConnection instance = DBConnection.Instance();
+            if (!instance.IsConnect())
+                instance.Connection?.Open();
+
+            /// Pour convertir une valeur normée en valeur originale = valeur normée * (borne max - borne min) + borne min
+            string query = "SELECT GROUP_CONCAT((valeur*(type_mesure.limite_max-type_mesure.limite_min)+type_mesure.limite_min)) as mesures, GROUP_CONCAT(instant) as instants, nom_type, unite_mesure FROM mesure JOIN appareil ON mesure.id_appareil = appareil.id_appareil JOIN type_mesure ON appareil.id_type = type_mesure.id_type GROUP BY appareil.id_appareil";
+            
+            Dictionary<string, MeasuresValues> values = new Dictionary<string, MeasuresValues>();
+
+            try {
+                using var command = new MySqlCommand(query, instance.Connection);
+                using var reader = command.ExecuteReader();
+                while (reader.Read()){
+                    values.Add(reader.GetString("unite_mesure"), new(){valeurs = reader.GetString("mesures"), instants = reader.GetString("instants"), type = reader.GetString("nom_type")});
+                }
+                return values;
+            } catch (MySqlException _) { ConsoleLogger.LogError("Impossible d'afficher les mesures de l'appareil " + idAppareil + " !"); }
+            return values;
+        }
 
         /// <summary>
         /// Ajoute une mesure dans la base de donnée si l'appareil associé est déjà enregistré.
